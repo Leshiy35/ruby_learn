ab4da01e6dcd6d5b864fceddf9935d5dbb9575c1
Alex Shchekin <leshiy35@gmail.com>
2016-03-25 16:18:15 +0300

    Added files via upload

---
+++ hashes.rb
@@ -0,0 +1,9 @@
+puts "hashes"
+hh = {'Mike' => '35373', 'Jessie' => '31380'}
+puts hh
+hh ['Walt'] = '32211'
+puts hh
+
+options = {:font_size => 10, :font_family => "Arial"}
+x  = options[:font_size]
+puts x
